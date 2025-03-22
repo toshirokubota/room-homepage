02802389c16f0a30e8a1578e19824703df6429fa
Toshiro Kubota <toshiro732@hotmail.com>
2025-03-22 14:35:46 -0400

    ready to submit

--- main.js
+++ main.js
@@ -10,77 +10,90 @@ captions.forEach(e => {
 
 let count = 0;
 const max_count = pictures.length;
-console.log(max_count, captions);
-console.log(carousel_txts);
+// console.log(max_count, captions);
+// console.log(carousel_txts);
 
 const updateCarousel = (count) => {
     let img_width = pictures[0].clientWidth;
     let caption_width = captions[0].clientWidth;
     let offset1 = count * img_width;
     let offset2 = count * caption_width;
-    console.log(offset1, offset2);
+    // console.log(offset1, offset2);
     carousel_pics.style.transform = `translateX(-${offset1}px)`;
     carousel_txts.style.transform = `translateX(-${offset2}px)`;
     for(let i=0; i<max_count; ++i) {
         let pic = pictures[i];
         let txt = captions[i];
         if(i==count) {
-            pic.tabIndex = 0;
             txt.shop_now.tabIndex = 0;
             pic.setAttribute('aria-hidden', 'false');
             txt.setAttribute('aria-hidden', 'false');
         } else {
-            pic.tabIndex = -1;
             txt.shop_now.tabIndex = -1;
             pic.setAttribute('aria-hidden', 'true');
             txt.setAttribute('aria-hidden', 'true');
         }
     }
 }
+
 //initial setup for the carousel
 updateCarousel(count);
 
-left_arrow.addEventListener('click', () => {
-    //console.log('left', count);
+const carouselNext = () => {
+    if(count < max_count - 1) {
+        count ++;
+        updateCarousel(count);
+    }
+}
+const carouselPrev = () => {
     if(count > 0) {
         count --;
         updateCarousel(count);
-        // let offset1 = count * img.clientWidth;
-        // let offset2 = count * caption.clientWidth;
-        // console.log(offset1, offset2, img.clientWidth);
-        // carousel_pics.style.transform = `translateX(-${offset1}px)`;
-        // carousel_txts.style.transform = `translateX(-${offset2}px)`;
+    }
+}
+
+left_arrow.addEventListener('click', () => {
+    carouselPrev();
+});
+left_arrow.addEventListener('keydown', (e)=> {
+    console.log('left keydown', e.code);
+    if(e.code == 'Enter') {
+        carouselPrev();
     }
 });
 
 right_arrow.addEventListener('click', () => {
-    //console.log('right', count);
-    if(count < max_count - 1) {
-        count ++;
-        updateCarousel(count);
-        // let offset1 = count * img.clientWidth;
-        // let offset2 = count * caption.clientWidth;
-        // console.log(offset1, offset2, img.clientWidth);
-        // carousel_pics.style.transform = `translateX(-${offset1}px)`;
-        // carousel_txts.style.transform = `translateX(-${offset2}px)`;
+    carouselNext();
+});
+right_arrow.addEventListener('keydown', (e)=> {
+    console.log('right keydown', e.code);
+    if(e.code == 'Enter') {
+        carouselNext();
     }
 });
 
+//collapsable menue
 const nav = document.getElementById('nav-bar');
+const hamburger = document.getElementById('hamburger-icon');
+const close_icon = document.getElementById('close-icon');
 const showMenu = () => {
     nav.classList.add('show');
 }
 const hideMenu = () => {
     nav.classList.remove('show');
 }
-document.getElementById('hamburger-icon').addEventListener('click', showMenu);
-document.getElementById('close-icon').addEventListener('click', hideMenu);
+hamburger.addEventListener('click', showMenu);
+hamburger.addEventListener('keydown', (e)=> {
+    if(e.code == 'Enter') {
+        showMenu();
+    }
+})
+close_icon.addEventListener('click', hideMenu);
+close_icon.addEventListener('keydown', (e) => {
+    if(e.code == 'Enter') {
+        hideMenu();
+    }
+});
 nav.querySelectorAll('.menu > a').forEach(elm => {
     elm.addEventListener('click', hideMenu)
 });
-
-
-// window.onresize = () => {
-//     let elm = document.querySelector('.carousel-item img');
-//     console.log(elm.clientWidth, elm.clientHeight);
-// }
