ebb83b07dcf7aebab0e4a13d540fd2aaf34bcd90
Toshiro Kubota <toshiro732@hotmail.com>
2025-03-21 20:48:07 -0400

    pretty much working!

--- main.js
+++ main.js
@@ -2,18 +2,22 @@ const left_arrow = document.getElementById('carousel-left-arrow');
 const right_arrow = document.getElementById('carousel-right-arrow');
 const carousel_pics = document.querySelector('.carousel-pictures');
 const carousel_txts = document.querySelector('.carousel-captions');
+const img = document.querySelector('.carousel-pictures picture');
+const caption = document.querySelector('.carousel-captions .caption');
 let count = 0;
 const max_count = document.querySelectorAll('.carousel-pictures > picture').length;
-console.log(max_count, left_arrow, right_arrow, carousel_pics, carousel_txts);
+//console.log(max_count, left_arrow, right_arrow, carousel_pics, carousel_txts);
+console.log(img);
 
 left_arrow.addEventListener('click', () => {
     console.log('left', count);
     if(count > 0) {
         count --;
-        let offset = count * window.innerWidth;
-        console.log(offset, window.innerWidth);
-        carousel_pics.style.transform = `translateX(${offset}px)`;
-        carousel_txts.style.transform = `translateX(${offset}px)`;
+        let offset1 = count * img.clientWidth;
+        let offset2 = count * caption.clientWidth;
+        console.log(offset1, offset2, img.clientWidth);
+        carousel_pics.style.transform = `translateX(-${offset1}px)`;
+        carousel_txts.style.transform = `translateX(-${offset2}px)`;
     }
 });
 
@@ -21,10 +25,11 @@ right_arrow.addEventListener('click', () => {
     console.log('right', count);
     if(count < max_count - 1) {
         count ++;
-        let offset = count * window.innerWidth;
-        console.log(offset, window.innerWidth);
-        carousel_pics.style.transform = `translateX(-${offset}px)`;
-        carousel_txts.style.transform = `translateX(-${offset}px)`;
+        let offset1 = count * img.clientWidth;
+        let offset2 = count * caption.clientWidth;
+        console.log(offset1, offset2, img.clientWidth);
+        carousel_pics.style.transform = `translateX(-${offset1}px)`;
+        carousel_txts.style.transform = `translateX(-${offset2}px)`;
     }
 });
 
